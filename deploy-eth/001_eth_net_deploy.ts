413d1b54d4949054e2edf940d2832d33e87446cd
Relayers <relayersdao@gmail.com>
2022-06-23 21:02:57 +0800

    deployed the eth main net

--- deploy-eth/001_eth_net_deploy.ts
+++ deploy-eth/001_eth_net_deploy.ts
@@ -12,27 +12,29 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const {deploy} = deployments;
 
 
-    let deployer1:SignerWithAddress,deployer2:SignerWithAddress;
     let address_torn_erc20 =  '0x77777FeDdddFfC19Ff86DB637967013e6C6A116C';
     let address_TornadoGovernanceStaking =  '0x5efda50f22d34f262c29268506c5fa42cb56a1ce';
     let address_RelayerRegistry =  '0x58E8dCC13BE9780fC42E8723D8EaD4CF46943dF2';
 
-
-    const {operator} = await getNamedAccounts();
-
     let users:USER_FIX = await get_user_fixture();
-    deployer1 = users.deployer1;
 
+
+
+    const contracts = {
+        mock_torn: address_torn_erc20,
+        mTornadoGovernanceStaking:address_TornadoGovernanceStaking,
+        mRelayerRegistry:address_RelayerRegistry,
+    };
 
     let ret_RootManger_logic =  await deploy('RootManger_logic', {
-        from: deployer1.address,
-        args: [address_RelayerRegistry,address_torn_erc20],
+        from: users.deployer1.address,
+        args: [contracts.mRelayerRegistry,contracts.mock_torn],
         log: true,
         contract:"RootManger"
     });
 
     let ret_RootManger =  await deploy('RootManger', {
-        from: deployer1.address,
+        from: users.deployer1.address,
         args: [ret_RootManger_logic.address,users.proxy_admin.address,"0x"],
         log: true,
         contract:"RelayerDAOProxy"
@@ -40,83 +42,68 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
 
     let ret_Income_logic =  await deploy('Income_logic', {
-        from: deployer1.address,
-        args: [address_torn_erc20,ret_RootManger.address],
+        from: users.deployer1.address,
+        args: [contracts.mock_torn,ret_RootManger.address],
         log: true,
         contract:"Income"
     });
 
+
     let ret_mIncome =  await deploy('Income', {
-        from: deployer1.address,
+        from: users.deployer1.address,
         args: [ret_Income_logic.address,users.proxy_admin.address,"0x"],
         log: true,
         contract:"RelayerDAOProxy"
     });
 
+
+
     let ret_mDeposit_logic =  await deploy('Deposit_logic', {
-        from: deployer1.address,
-        args: [address_torn_erc20,address_TornadoGovernanceStaking,address_RelayerRegistry,ret_RootManger.address],
+        from: users.deployer1.address,
+        args: [contracts.mock_torn,contracts.mTornadoGovernanceStaking,contracts.mRelayerRegistry,ret_RootManger.address],
         log: true,
         contract:"Deposit"
     });
 
+
     let ret_Deposit =  await deploy('Deposit', {
-        from: deployer1.address,
+        from: users.deployer1.address,
         args: [ret_mDeposit_logic.address,users.proxy_admin.address,"0x"],
         log: true,
         contract:"RelayerDAOProxy"
     });
 
-    let ret_mExitQueue_logic =  await deploy('ExitQueue_logic', {
-        from: deployer1.address,
-        args: [address_torn_erc20,ret_RootManger.address],
-        log: true,
-        contract:"ExitQueue"
-    });
 
-    let ret_mExitQueue =  await deploy('ExitQueue', {
-        from: deployer1.address,
-        args: [ret_mExitQueue_logic.address,users.proxy_admin.address,"0x"],
-        log: true,
-        contract:"RelayerDAOProxy"
-    });
-
-
-    let ret_profitRecord_logic =  await deploy('profitRecord_logic', {
-        from: deployer1.address,
-        args: [address_torn_erc20,ret_RootManger.address],
+    let ret_ProfitRecord_logic =  await deploy('ProfitRecord_logic', {
+        from: users.deployer1.address,
+        args: [contracts.mock_torn,ret_RootManger.address],
         log: true,
         contract:"ProfitRecord"
     });
 
-    let ret_profitRecord =  await deploy('profitRecord', {
-        from: deployer1.address,
-        args: [ret_profitRecord_logic.address,users.proxy_admin.address,"0x"],
+    let ret_ProfitRecord =  await deploy('ProfitRecord', {
+        from: users.deployer1.address,
+        args: [ret_ProfitRecord_logic.address,users.proxy_admin.address,"0x"],
         log: true,
         contract:"RelayerDAOProxy"
     });
 
 
-    if(ret_RootManger.newlyDeployed){
-        let mRootManger = <RootManger>await (await ethers.getContractFactory("RootManger")).attach(ret_RootManger.address);
-        await mRootManger.connect(users.owner).__RootManger_init(ret_mIncome.address, ret_Deposit.address, ret_mExitQueue.address,ret_profitRecord.address);
-        await mRootManger.connect(users.owner).setOperator(operator);
-    }
 
-   if(ret_Deposit.newlyDeployed){
-       let  mDeposit = <Deposit>await (await ethers.getContractFactory("Deposit")).attach(ret_Deposit.address);
-       await mDeposit.connect(users.owner).__Deposit_init();
-   }
+    let ret_mExitQueue_logic =  await deploy('ExitQueue_logic', {
+        from: users.deployer1.address,
+        args: [contracts.mock_torn,ret_RootManger.address],
+        log: true,
+        contract:"ExitQueue"
+    });
 
-    if(ret_mExitQueue.newlyDeployed){
-        let mExitQueue = <ExitQueue>await (await ethers.getContractFactory("ExitQueue")).attach(ret_mExitQueue.address);
-        await mExitQueue.connect(users.owner).__ExitQueue_init();
-    }
 
-    if(ret_profitRecord.newlyDeployed){
-        let mProfitRecord = <ProfitRecord>await (await ethers.getContractFactory("ProfitRecord")).attach(ret_profitRecord.address);
-        await mProfitRecord.connect(users.owner).__ProfitRecord_init();
-    }
+    let ret_mExitQueue =  await deploy('ExitQueue', {
+        from: users.deployer1.address,
+        args: [ret_mExitQueue_logic.address,users.proxy_admin.address,"0x"],
+        log: true,
+        contract:"RelayerDAOProxy"
+    });
 
 };
 export default func;
--- scripts/eth_net_deploy_check.ts
+++ scripts/eth_net_deploy_check.ts
@@ -1,4 +1,4 @@
-import {Deposit, ExitQueue, RootManger} from "../typechain-types";
+import {Deposit, ExitQueue, ProfitRecord, RootManger} from "../typechain-types";
 
 import {get_user_fixture, USER_FIX} from "../test/start_up";
 
@@ -8,38 +8,71 @@ const { ethers } = require("hardhat");
 async function main() {
 
     const contracts = {
-        Deposit:"",
-        RootManger:"",
-        ExitQueue:"",
-        Income:"",
-        profitRecord:"",
+        Deposit:"0x7155F8c9B74BED0C69d58A730914f2232c5CeE4c",
+        RootManger:"0x6201dBf488e977018F4A1BbD8Df57F5327991ECE",
+        ExitQueue:"0x859971C83B596e599D1C00254E827d7de90a0562",
+        Income:"0xD184DE1fAD8F2D340c7720A3e1D54f5947e010e5",
+        profitRecord:"0xb1eB9a41d45b5CDEf4ca8586e05Cc5C001AAEdB8",
     };
 
     let mRootManger = <RootManger>await (await ethers.getContractFactory("RootManger")).attach(contracts.RootManger);
     let  mDeposit = <Deposit>await (await ethers.getContractFactory("Deposit")).attach(contracts.Deposit);
     let mExitQueue = <ExitQueue>await (await ethers.getContractFactory("ExitQueue")).attach(contracts.ExitQueue);
-
+    let mProfitRecord = <ProfitRecord>await (await ethers.getContractFactory("ProfitRecord")).attach(contracts.profitRecord);
     let users:USER_FIX = await get_user_fixture();
 
 
-    if((await mRootManger.connect(users.owner).owner()) != users.owner.address){
+
         console.log("__RootManger_init");
-        await mRootManger.connect(users.owner).__RootManger_init(contracts.Income, contracts.Deposit, contracts.ExitQueue,contracts.profitRecord);
-        await mRootManger.connect(users.owner).setOperator(users.operator.address);
+        try {
+           let tx = await mRootManger.connect(users.owner).__RootManger_init(contracts.Income, contracts.Deposit, contracts.ExitQueue,contracts.profitRecord);
+           await tx.wait(1);
+        } catch (e:any) {
+            console.log(e.reason)
+        }
+
+
+    if((await mRootManger.operator()) != users.operator.address){
+        try {
+            console.log("setOperator")
+           let tx = await mRootManger.connect(users.owner).setOperator(users.operator.address);
+           await tx.wait(1);
+        } catch (e:any) {
+            console.log(e.reason)
+        }
+
     }
 
-    if((await mDeposit.connect(users.owner).EXIT_QUEUE()) != contracts.ExitQueue){
-        console.log("__Deposit_init");
-        await mDeposit.connect(users.owner).__Deposit_init();
-    }
 
-    //there is no way to detect init
-    try {
+        try {
+            console.log("__Deposit_init");
+            let tx = await mDeposit.connect(users.owner).__Deposit_init();
+            await tx.wait(1);
+        }  catch (e:any) {
+            console.log(e.reason)
+        }
+
+
+
         console.log("__ExitQueue_init");
-        await mExitQueue.connect(users.owner).__ExitQueue_init();
-    } catch (e:any) {
-        console.log(e.reason);
-    }
+        try {
+           let tx=  await mExitQueue.connect(users.owner).__ExitQueue_init();
+            await tx.wait(1);
+        } catch (e:any) {
+            console.log(e.reason)
+        }
+
+        console.log("__mProfitRecord_init");
+        try {
+            let tx=  await mProfitRecord.connect(users.owner).__ProfitRecord_init();
+            await tx.wait(1);
+        } catch (e:any) {
+            console.log(e.reason)
+        }
+
+
+
+
 }
 
 main()
