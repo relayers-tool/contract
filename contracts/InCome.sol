4730e1abda9b3ba9f1e72ec135f735bc0036789f
Relayers <relayersdao@gmail.com>
2022-06-27 15:21:19 +0800

    del some code

--- contracts/InCome.sol
+++ contracts/InCome.sol
@@ -1,40 +1,35 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
 import "./RootDB.sol";
-contract Income is  ContextUpgradeable{
-    using SafeERC20Upgradeable for IERC20Upgradeable;
+contract Income {
+
     address immutable public TORN_CONTRACT;
     address immutable public ROOT_DB;
 
+    event distribute_torn(address,uint256);
 
-    /** ---------- constructor ---------- **/
+
     constructor(
-        address _tornContract,
+        address _torn_contract,
         address _root_db
     ) {
-        TORN_CONTRACT = _tornContract;
+        TORN_CONTRACT = _torn_contract;
         ROOT_DB = _root_db;
     }
 
-    /** ---------- modifier ---------- **/
     modifier onlyOperator() {
         require(msg.sender == RootDB(ROOT_DB).operator(), "Caller is not operator");
         _;
     }
 
 
-    event distribute_torn(address,uint256);
     function distributeTorn(uint256 _amount) external onlyOperator {
-        //require(IERC20Upgradeable(TORN_CONTRACT).balanceOf(address(this)) >= _amount, 'Insufficient balance');
-        address _depositAddress = RootDB(ROOT_DB).depositContract();
-        //require(_depositAddress != address(0), 'Invalid deposit address');
+        address deposit_address = RootDB(ROOT_DB).depositContract();
         RootDB(ROOT_DB).addIncome(_amount);
-        SafeERC20Upgradeable.safeTransfer(IERC20Upgradeable(TORN_CONTRACT),_depositAddress, _amount);
-        emit distribute_torn(_depositAddress,_amount);
+        SafeERC20Upgradeable.safeTransfer(IERC20Upgradeable(TORN_CONTRACT), deposit_address, _amount);
+        emit distribute_torn(deposit_address,_amount);
     }
 
 
