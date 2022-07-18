078522958c06d73742891c437b7de53eb6cfc066
Relayers <relayersdao@gmail.com>
2022-07-18 10:30:43 +0800

    change compiler version to 0.8.9

--- contracts/Deposit.sol
+++ contracts/Deposit.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.9;
 import "./Interface/ITornadoStakingRewards.sol";
 import "./Interface/ITornadoGovernanceStaking.sol";
 import "./Interface/IRelayerRegistry.sol";
